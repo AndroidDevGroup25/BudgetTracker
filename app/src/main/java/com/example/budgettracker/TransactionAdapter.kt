7249d564390828b366094eb10011afebf8bd15f2
lauren-nguyen23 <tranghanguyen242@gmail.com>
2022-04-23 00:07:55 -0400

    Added DetailActivity to display info on the transaction that was clicked on from SummaryFragment

--- app/src/main/java/com/example/budgettracker/TransactionAdapter.kt
+++ app/src/main/java/com/example/budgettracker/TransactionAdapter.kt
@@ -1,23 +1,29 @@
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.budgettracker.DetailActivity
 import com.example.budgettracker.R
 import com.example.budgettracker.Transaction
 
-/*BEGINNING OF ADAPTER CLASS*/
+
+const val TRANSACTION_EXTRA = "TRANSACTION_EXTRA"
+
+private const val TAG= "TransactionAdapter"
+
 class TransactionAdapter(val context: Context, val transactions: MutableList<Transaction>): RecyclerView.Adapter<TransactionAdapter.ViewHolder>(){
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val tvTransactionName: TextView
-        val tvCreatedAt: TextView
-        val tvCost : TextView
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
+        private val tvTransactionName = itemView.findViewById<TextView>(R.id.tvTransactionName)
+        private val tvCreatedAt = itemView.findViewById<TextView>(R.id.tvCreatedAt)
+        private val tvCost = itemView.findViewById<TextView>((R.id.tvCost))
 
         init {
-            tvTransactionName = itemView.findViewById(R.id.tvTransactionName)
-            tvCreatedAt = itemView.findViewById(R.id.tvCreatedAt)
-            tvCost = itemView.findViewById((R.id.tvCost))
+            itemView.setOnClickListener(this)
         }
         fun bind(transaction: Transaction){
             //todo should we include usernames in the adapter?
@@ -26,15 +32,27 @@ class TransactionAdapter(val context: Context, val transactions: MutableList<Tra
             tvCost.text = transaction.getCost().toString()
             tvCreatedAt.text = transaction.getDate().toString()
         }
+
+        override fun onClick(p0: View?) {
+            //1. Get notified of the particular transaction that was clicked on
+            val transaction = transactions[adapterPosition]
+            //Toast.makeText(context, transaction.getDescription(), Toast.LENGTH_SHORT).show()
+            //2. Create an intent to navigate to the DetailActivity (detail view for transaction)
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(TRANSACTION_EXTRA, transaction)
+            context.startActivity(intent)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionAdapter.ViewHolder {
+        Log.i(TAG, "onCreateViewHolder")
         val view = LayoutInflater.from(context).inflate(R.layout.item_transaction, parent,false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TransactionAdapter.ViewHolder, position: Int) {
-        val transaction = transactions.get(position)
+        Log.i(TAG, "onBindViewHolder position $position")
+        val transaction = transactions[position]
         holder.bind(transaction)
     }
 
